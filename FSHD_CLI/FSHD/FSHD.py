9dc3d7cb83a6906344da1032e2a0cc23ff2749f0
AntiCakejesCult <72666992+AntiCakejesCult@users.noreply.github.com>
2023-06-13 12:32:59 +0200

    add list of datasets to CLI

--- FSHD_CLI/FSHD/FSHD.py
+++ FSHD_CLI/FSHD/FSHD.py
@@ -1,11 +1,23 @@
-import argparse
-from argparse import RawTextHelpFormatter
-from itertools import product
 import sys
 import json
-import pkg_resources
-from pathlib import Path
 import shutil
+import argparse
+import pkg_resources
+import questionary
+from questionary import Style
+from pathlib import Path
+from itertools import product
+from argparse import RawTextHelpFormatter
+
+custom_style = Style([
+    ('separator', 'fg:#6C6C6C'),
+    ('qmark', 'fg:#FF9D00 bold'),  # question mark at the start of the prompt
+    ('question', 'fg:#9ad4f5'),  # question text
+    ('selected', 'fg:#5F819D'),  # for the selected item
+    ('pointer', 'fg:#fff bold'),  # pointer used in select and checkbox prompts
+    ('answer', 'fg:#fe2e2e bold'),  # for answer 
+    ('highlighted', 'fg:#a6e0ff bold'),
+])
 
 def get_abbrivations():
     """Fetches the abbrivations from abbrivations.json
@@ -47,7 +59,8 @@ def validate_combinations(args):
         True if there is a valid combination of arguments. False if there is there are no combinations found.
     """
     valid_combinations = [
-        args.abbriviations,  
+        args.abbriviations,
+        args.list_datasets and args.selection, 
         args.selection and args.ethnicity, 
         args.haplotypes and args.ethnicity,
         args.selection and args.input,
@@ -114,20 +127,21 @@ def haplo(args):
     return (", ".join(unique_haplotypes))
 
 
-def predict(p_selection, p_region, input_type):
+def predict(args):
     """
     haplotypes = [[EU4, EU10], [AF4, AF10], [AS4, AS10]]
     """
-    if input_type:
-        with open(input_type, 'r') as f:
+    if args.input:
+        with open(args.input, 'r') as f:
             ethnicity_haplotypes = json.load(f)
+    elif args.hidden_dataset is not None:
+        ethnicity_haplotypes = args.hidden_dataset
     else:
-        ethnicity_haplotypes = get_ethnicity(p_region)
-    
+        ethnicity_haplotypes = get_ethnicity(args.ethnicity)
     
     reeksvier, reekstien, kansenreeks = [],[],[]
     totaalkans = 0
-    for nr in set(p_selection):
+    for nr in set(args.selection):
         nr = str(nr)
         for chr in ethnicity_haplotypes:
             try:
@@ -138,11 +152,10 @@ def predict(p_selection, p_region, input_type):
             except LookupError as e:
                 pass
 
-
     #-> alle mogelijkheden genereren
     for i,j,k,l in product(reeksvier, reeksvier, reekstien, reekstien):
         nummers = [int(i["haplotype"][1:4]), int(j["haplotype"][1:4]), int(k["haplotype"][1:4]), int(l["haplotype"][1:4])]
-        if sorted(nummers) == p_selection: #checken of ze wel uniek zijn (of de nummers kloppen met het voorbeeld)                   
+        if sorted(nummers) == args.selection: #checken of ze wel uniek zijn (of de nummers kloppen met het voorbeeld)                   
             kans = (float(i["%"]) * float(j["%"]) * float(k["%"]) * float(l["%"]))/1000000
             berekening = i["%"] + ' * ' + j["%"] + ' * ' + k["%"] + ' * ' + l["%"] 
             genotype = ['4' + i["haplotype"], '4' + j["haplotype"], '10' + k["haplotype"], '10' + l["haplotype"]]
@@ -182,7 +195,21 @@ def other_options(args, option_string):
     if args.abbriviations:
         print(f"The possible abbrivations are: {option_string}", end="")
         sys.exit()
-        
+    
+    if args.list_datasets:
+        ethnicity_directory = Path(pkg_resources.resource_filename(__name__, "ethnicity"))
+        dataset_dict = {file.name: file for file in ethnicity_directory.iterdir()} 
+        selected_dataset = questionary.select(
+            "Select a dataset",
+            choices=list(dataset_dict.keys()),
+            style=custom_style,
+            pointer="❯",
+            use_jk_keys=True,
+            show_selected=True,
+            qmark=""
+        ).ask() 
+        with open(dataset_dict[selected_dataset]) as dataset:
+            args.hidden_dataset = json.load(dataset)
     if args.add and args.input and args.file_abbreviation:
         to_ethnicity(args.input)
         with open(pkg_resources.resource_filename(__name__, "abbreviations.json"), "r+") as file:
@@ -192,12 +219,12 @@ def other_options(args, option_string):
         with open(pkg_resources.resource_filename(__name__, "abbreviations.json"), "w") as new_file: 
             new_file.write(json.dumps(abbr_dict, indent=4))
         sys.exit()
+    
 
 def run(args):
-    p_selection = sorted(args.selection)
-    p_ethnicity = args.ethnicity
+    args.selection = sorted(args.selection)
     separator = {'t': '\t', ',': ',', '|': '|', ';': ';', ' ': ' '}.get(args.separator)
-    result, totaalkans = predict(p_selection, p_ethnicity, args.input)
+    result, totaalkans = predict(args)
     if result != 1 and totaalkans != 1:
         tot_perm = [100*i[0]/totaalkans for i in result if i[2] > 0]
         output = args.output is not None
@@ -212,7 +239,7 @@ def run(args):
         else:
             print(f"Total Likelihood: {sum(tot_perm):.1f}")
     else:
-        print(f"No results found for this selection {' '.join(str(i) for i in p_selection)} for ethnicity {p_ethnicity}!\n"
+        print(f"No results found for this selection {' '.join(str(i) for i in args.selection)} for ethnicity {args.ethnicity}!\n"
               f"For more information use the -h option 'python haplotype.py -h'.")
         
 def parser_args():
@@ -226,7 +253,27 @@ def parser_args():
         option_string: string: String which says what abbrivations are possible and what they stand 
 _
     """
-    parser = argparse.ArgumentParser(description="A tool for predicting genotypes from SSLP values for FSHD analysis. This tool allows users to input specific SSLP selections, specify the ethnicity of the patient, and also provides the ability to add new haplotype files.", formatter_class=RawTextHelpFormatter)
+    a = """
+
+                                                                       ,--,            
+                              ,--,                                  ,---.'|            
+    ,---,.  .--.--.         ,--.'|    ,---,                ,----..  |   | :      ,---, 
+  ,'  .' | /  /    '.    ,--,  | :  .'  .' `\             /   /   \ :   : |   ,`--.' | 
+,---.'   ||  :  /`. / ,---.'|  : ',---.'     \     ,---,.|   :     :|   ' :   |   :  : 
+|   |   .';  |  |--`  |   | : _' ||   |  .`\  |  ,'  .' |.   |  ;. /;   ; '   :   |  ' 
+:   :  :  |  :  ;_    :   : |.'  |:   : |  '  |,---.'   ,.   ; /--` '   | |__ |   :  | 
+:   |  |-, \  \    `. |   ' '  ; :|   ' '  ;  :|   |    |;   | ;    |   | :.'|'   '  ; 
+|   :  ;/|  `----.   \'   |  .'. |'   | ;  .  |:   :  .' |   : |    '   :    ;|   |  | 
+|   |   .'  __ \  \  ||   | :  | '|   | :  |  ':   |.'   .   | '___ |   |  ./ '   :  ; 
+'   :  '   /  /`--'  /'   : |  : ;'   : | /  ; `---'     '   ; : .'|;   : ;   |   |  ' 
+|   |  |  '--'.     / |   | '  ,/ |   | '` ,/            '   | '/  :|   ,/    '   :  | 
+|   :  \    `--'---'  ;   : ;--'  ;   :  .'              |   :    / '---'     ;   |.'  
+|   | ,'              |   ,/      |   ,.'                 \   \ .'            '---'    
+`----'                '---'       '---'                    `---`                       
+                                                                                       
+
+"""
+    parser = argparse.ArgumentParser(description=f"{a}\nA tool for predicting genotypes from SSLP values for FSHD analysis. This tool allows users to input specific SSLP selections, specify the ethnicity of the patient, and also provides the ability to add new haplotype files.", formatter_class=RawTextHelpFormatter)
     group1 = parser.add_argument_group('required arguments')
     group1.add_argument("-s", "--selection", nargs="+", type=int, required=False, metavar="SSLP",
                     help="Provide a list of four integer SSLP values. Example usage: -s 159 161 163 166.")
@@ -244,12 +291,15 @@ _
                         help="Provide the path to a custom input JSON file containing haplotypes.")
     group2.add_argument("-o", "--output", type=str, metavar="OUTPUT FILE",
                         help="Specify the path to an output file where the results will be written.\nIf not specified, results will be printed to the console.")
-    group2.add_argument("-sep", "--separator", type=str, choices=[",", "t", "|", ";", " "], metavar="SEPARATOR",
+    group2.add_argument("-sep", "--separator", type=str, choices=[",", "t", "|", ";", " "], default=",", metavar="SEPARATOR",
                         help="Choose a separator for the output file.\nOptions include comma (,), tab (t), pipe (|), semicolon (;), or space ( ). Default is comma ','.")
     group2.add_argument("-A", "--add", action="store_true",
                         help="Use this flag in combination with -i/--input and -Fa/--file_abbreviation to add a new haplotype file.")
     group2.add_argument("-Fa", "--file_abbreviation", type=str, metavar="FILE ABBREVIATION",
                         help="When adding a new haplotype file, specify the abbreviation to be associated with this file.")
+    group2.add_argument("-l", "--list_datasets", action="store_true",
+                        help="Shows a list of all the haplotype datasets available in the ethnicity directory.")
+    
     args = parser.parse_args()
     return args, parser,  ethnicity_full, option_string
     
@@ -258,12 +308,14 @@ _
 def main():
     args, parser, ethnicity_full, option_string = parser_args()
     args.ethnicity = ethnicity_full.get(args.ethnicity, args.ethnicity)
+    args.hidden_dataset = None
     if args.selection and len(args.selection) !=  4:
         parser.error(f"You must have a input of 4 SSLP's. The length of your input is {len(args.selection)} SSLP's.")
     if validate_combinations(args):
         parser.error("Invalid combination of arguments. You must provide one of the following combinations:\n"
                      "-a/--abbriviations\n"
                      "-s/--selection and -e/--ethnicity\n"
+                     "-s/--selection and -l/--list_datasets\n"
                      "-s/--selection and -i/--input\n"
                      "-h/--haplotypes and -e/--ethnicity\n"
                      "-h/--haplotypes and -i/--input\n"
