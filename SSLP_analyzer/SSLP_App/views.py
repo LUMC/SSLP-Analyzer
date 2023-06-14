29f3ab014a1063a8b79005f354daf80a04830ace
Tim <s1126607@student.hsleiden.nl>
2023-06-14 14:43:51 +0200

    dummy data for table + html render

--- SSLP_analyzer/SSLP_App/views.py
+++ SSLP_analyzer/SSLP_App/views.py
@@ -4,7 +4,9 @@ def home_view(request):
     return render(request, 'homepage.html')
 
 def data_editor_view(request):
-    return render(request, 'editpage.html')
+    
+    dummy = [{"haplo":"Test", "chr":"4","SSLP":"162","percent":"0.60","perm":"1"} for _ in range(25)]
+    return render(request, 'editpage.html',{"table":dummy})
 
 def feed_view(request):
     return render(request, 'login.html')
