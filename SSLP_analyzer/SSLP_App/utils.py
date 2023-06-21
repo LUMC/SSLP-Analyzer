2fae8adfcd934ec393d7e54dc5df5c809bd1ed4e
Tim <s1126607@student.hsleiden.nl>
2023-06-21 12:17:12 +0200

    json_parser

--- SSLP_analyzer/SSLP_App/utils.py
+++ SSLP_analyzer/SSLP_App/utils.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
-from os import getcwd
 import re
+import json
 
 
 def haplo_parser(haplotype):
-    chr,sslp = [int(num) for num in re.findall('\d+', haplotype)]
+    chromosome, sslp = [int(num) for num in re.findall('\d+', haplotype)][0:2]
     haplo_string = re.sub("^\d+", '', haplotype)
-    return haplo_string,chr,sslp
+    return haplo_string,chromosome,sslp
 
 
 
@@ -16,20 +16,40 @@ def xslx_parser(filename):
     df_list = np.split(df, df[df.isnull().all(1)].index)
     for i, chrs in enumerate(df_list):
         df_list[i] = chrs.dropna(how='all').reset_index(drop=True)[1:-1].drop(columns=1)
-    
-    chr_dict = {}
-    for chrs in df_list:
-        print(chrs.values.tolist())
-        break
+    chr_dict = {"haplotype":[],"chr":[],"SSLP":[],"%":[],"permissive":[]}
+    for dfs in df_list:
+        for row in dfs.values.tolist():
+            raw_haplo, percent, permissive = row
+            haplotype, chromosome, sslp = haplo_parser(raw_haplo)
+            chr_dict["haplotype"].append(haplotype)
+            chr_dict["chr"].append(chromosome)
+            chr_dict["SSLP"].append(sslp)
+            chr_dict["%"].append(percent)
+            chr_dict["permissive"].append(permissive)
+    return pd.DataFrame(chr_dict)
         
-    
-    print(df_list[0])
-    haplo_parser("10A161S")
         
-
-
-    # print(df_list)
+def json_parser(df):
+    df_list = df.values.T.tolist()
+    result_dict = {}
+    for haplo,chr_num,sslp,percent,perm in zip(*df_list):
+        chromosome = f"chr{chr_num}"
+        if not chromosome in result_dict:
+            result_dict[chromosome] = {}
+        if sslp not in result_dict[chromosome]:
+            result_dict[chromosome][sslp] = []
+        result_dict[chromosome][sslp].append({"haplotype":haplo,"%":percent*100,"permissive":perm})
+    return json.dumps(result_dict,indent=4)
+        
+            
+            
+        
+def main(filename):
+    df = xslx_parser(filename)
+    json_string = json_parser(df)
+     
+        
     
     
 if __name__ == "__main__":
-    xslx_parser("india.xlsx")
+    main("india.xlsx")
