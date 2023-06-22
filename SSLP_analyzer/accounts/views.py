3e4bb2db6b68c82908e4d64ad52c367677fd66b9
Saladin Ajouaou <100775443+Salah3211@users.noreply.github.com>
2023-06-22 12:56:22 +0200

    deleted register

--- SSLP_analyzer/accounts/views.py
+++ SSLP_analyzer/accounts/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm ,UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def login_and_register_view(request):
     if request.method == 'POST':
         login_form = AuthenticationForm(request, data=request.POST)
-        register_form = UserCreationForm(request.POST or None)
         if 'login_form' in request.POST:
             if login_form.is_valid():
                 user = login_form.get_user()
@@ -16,30 +15,14 @@ def login_and_register_view(request):
                 messagesl = get_error_message(login_form)
                 context = {
                 "login_form": login_form,
-                "register_form": register_form,
                 "messagesl": messagesl,
                 }
                 print(messagesl)
                 return render(request, "accounts/signin.html", context)
-        elif 'register_form' in request.POST:
-            if register_form.is_valid():
-                user_object = register_form.save()
-                login(request, user_object)
-                return redirect("/")
-            else:
-                messagesr = get_error_message(register_form)
-                context = {
-                "login_form": login_form,
-                "register_form": register_form,
-                "messagesr": messagesr,
-                }
-                return render(request, "accounts/signin.html", context)
     else:
         login_form = AuthenticationForm(request, data=request.POST)
-        register_form = UserCreationForm(request.POST or None)
         context = {
             "login_form": login_form,
-            "register_form": register_form,
         }
         return render(request, "accounts/signin.html", context)
     
