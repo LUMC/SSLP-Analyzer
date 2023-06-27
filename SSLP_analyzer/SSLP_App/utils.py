cbf3c48b931807046c23a9a67d52bd28707546c1
Tim <s1126607@student.hsleiden.nl>
2023-06-27 12:55:33 +0200

    documentation for file export functions

--- SSLP_analyzer/SSLP_App/utils.py
+++ SSLP_analyzer/SSLP_App/utils.py
@@ -5,11 +5,24 @@ import json
 
 
 def haplo_parser(haplotype):
+    """halpo_parser
+    Parses the haplotype string and extracts the chromosome value and the sslp
+    It does this by extracting the first two ints.
+
+    :param haplotype: haplotype string
+    :return: (str,int,int) haplotype_string, chromosome , sslp
+    """
     chromosome, sslp = [int(num) for num in re.findall('\d+', haplotype)][0:2]
     haplo_string = re.sub("^\d+", '', haplotype)
     return haplo_string,chromosome,sslp
 
 def xslx_parser(filename):
+    """xslx_parser
+    Reads the supplied xslx file and parses it so it can be saved to haplotypes.json.
+    It returns a pandas dataframe which contains the data.
+    :param filename: (str) the name of the file to save it as
+    :return: df (pd.DataFrame) dataframe with all read in data.
+    """
     df = pd.read_excel(filename,header=None)
     df_list = np.split(df, df[df.isnull().all(1)].index)
     for i, chrs in enumerate(df_list):
@@ -32,6 +45,13 @@ def xslx_parser(filename):
         
         
 def json_parser(df):
+    """json_parser
+    Takes in a pandas dataframe and converts this to a json string which can be saved 
+    to haplotypes.json.
+
+    :param df: (pd.DataFrame) pandas dataframe containing the data
+    :return: (str) json.dumps string which is in same format as haplotypes.json
+    """
     df_list = df.values.T.tolist()
     result_dict = {}
     for haplo,chr_num,sslp,percent,perm in zip(*df_list):
@@ -44,6 +64,12 @@ def json_parser(df):
     return json.dumps(result_dict,indent=4)
 
 def export_xslx(population):
+    """export_xslx
+    Reads the selected population from a file and 
+    saves this data to result.xlsx. This is then saved to result.xlsx so it can later be served to the user.
+
+    :param population: (str) the selected population
+    """
     with open("haplotypes.json","r") as file:
         haplotypes = json.load(file)
     save_list = []
