f51061f76198ef9bb6f22161685f6a698737e590
Tim <s1126607@student.hsleiden.nl>
2023-07-04 15:07:59 +0200

    added .bat files for starting up

---
+++ runwebsite.bat
@@ -0,0 +1,38 @@
+REM Create a virtual environment
+echo Creating Python virtual environment...
+python -m venv sslp_env
+
+REM Activate the virtual environment
+echo Activating Python virtual environment...
+call sslp_env\Scripts\activate.bat
+
+REM Upgrade pip
+echo Upgrading pip...
+python -m pip install --upgrade pip
+
+REM Install packages from requirements.txt
+echo Installing required Python packages...
+pip install -r requirements.txt
+
+REM Change directory to Django project
+echo Changing directory to Django project...
+cd SSLP_analyzer
+
+REM Run Django server
+echo Starting Django server...
+start /min python manage.py runserver
+
+REM Open Chrome to Django URL
+echo Opening Django URL in Chrome...
+start chrome http://127.0.0.1:8000/
+
+
+REM Deactivate the virtual environment
+echo Deactivating Python virtual environment...
+call sslp_env\Scripts\deactivate.bat
+
+echo Done.
+
+pause
+
+exit
