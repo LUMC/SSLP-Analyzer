01d21091608a6d17c0be5d32ec146faa3d67e67d
Tim <s1126607@student.hsleiden.nl>
2023-07-05 13:02:58 +0200

    added user creation to installation script

--- run_site.bat
+++ run_site.bat
@@ -11,19 +11,15 @@ echo Starting Django server...
 start /min python manage.py runserver
 
 
-
 REM Open Chrome to Django URL
 echo Opening Django URL in Chrome...
 start chrome http://127.0.0.1:8000/
 
-REM Deactivate the virtual environment
-echo Deactivating Python virtual environment...
-call path\to\your\env\Scripts\deactivate.bat
 cd ..
 
 REM Wait for user input
-echo Done.
-
+echo Done. Server started and website opened!
+pause
 
 REM End of script
 exit
