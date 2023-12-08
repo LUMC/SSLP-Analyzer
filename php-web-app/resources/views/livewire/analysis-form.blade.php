4cd6a5346151bb610dffcce5d3edda7a18fdb2a3
Ivo Fokkema <i.f.a.c.fokkema@lumc.nl>
2023-12-08 16:16:18 +0100

    Adjust for Tailwind 2; use only colors defined by v2.

--- php-web-app/resources/views/livewire/analysis-form.blade.php
+++ php-web-app/resources/views/livewire/analysis-form.blade.php
@@ -2,16 +2,16 @@
                     <form wire:submit.prevent="predict">
                         <div class="flex justify-between">
                             <div class="py-2 sm:px-1 md:px-2 first:pl-0 last:pr-0">
-                                <select name="population" id="population" wire:model.lazy="sPopulation" class="rounded-md p-2 sm:px-2 md:px-4 bg-sky-100 shadow-sm ring-1 ring-inset ring-sky-300 sm:text-sm md:text-base @error('sPopulation') bg-rose-300 ring-red-600 @enderror">
+                                <select name="population" id="population" wire:model.lazy="sPopulation" class="rounded-md p-2 sm:px-2 md:px-4 bg-blue-100 shadow-sm ring-1 ring-inset ring-blue-300 sm:text-sm md:text-base @error('sPopulation') bg-red-300 ring-red-600 @enderror">
                                     <option value="">(Population)</option>
-                                    @foreach($this->aPopulations as $sPopulation)
+@foreach($this->aPopulations as $sPopulation)
                                         <option value="{{ $sPopulation }}">{{ $sPopulation }}</option>
-                                    @endforeach
+@endforeach
                                 </select>
                             </div>
 @for ($i = 1; $i <= 4; $i++)
                                 <div class="py-2 sm:px-1 md:px-2 first:pl-0 last:pr-0">
-                                    <select name="SSLP{{ $i }}" id="SSLP{{ $i }}" wire:model.defer="nSSLP{{ $i }}" class="rounded-md p-2 sm:px-2 md:px-4 bg-sky-100 shadow-sm ring-1 ring-inset ring-sky-300 sm:text-sm md:text-base @error('nSSLP' . $i) bg-rose-300 ring-red-600 @enderror">
+                                    <select name="SSLP{{ $i }}" id="SSLP{{ $i }}" wire:model.defer="nSSLP{{ $i }}" class="rounded-md p-2 sm:px-2 md:px-4 bg-blue-100 shadow-sm ring-1 ring-inset ring-blue-300 sm:text-sm md:text-base @error('nSSLP' . $i) bg-red-300 ring-red-600 @enderror">
                                         <option value="">(SSLP size)</option>
 @foreach($this->aSSLPs as $nSSLP)
                                         <option value="{{ $nSSLP }}">{{ $nSSLP }}</option>
